31ff558c74d899139cabe6aff29e8c329bb8adf5
Naveen B <naveen.dec29@gmail.com>
2023-11-11 18:17:15 -0500

    [refactor] /-/-:: dcccommit1

--- src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLazyInitExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLazyInitExampleApplication.class, args);
+		SpringApplication.run(SpringBootLazyInitExampleApplication.class, args)
 	}
 
 }
