575024ad998f10a94a35ac7c11c76aabf6a63875
Naveen B <naveen.dec29@gmail.com>
2023-11-11 20:36:41 -0500

    [refactor] annotation updated

--- src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
@@ -3,7 +3,7 @@ package com.naveen.lazy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplicationnotationService
 public class SpringBootLazyInitExampleApplication {
 
 	public static void main(String[] args) {
