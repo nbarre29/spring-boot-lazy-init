31218ff9ddf0ea5684604c3888440f306676f27a
Naveen B <naveen.dec29@gmail.com>
2023-11-11 20:34:48 -0500

    [main] removed annotation

--- src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
@@ -3,7 +3,7 @@ package com.naveen.lazy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class SpringBootLazyInitExampleApplication {
 
 	public static void main(String[] args) {
