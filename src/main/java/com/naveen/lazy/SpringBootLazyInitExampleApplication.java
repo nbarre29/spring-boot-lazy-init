344778dfd84fe12b080eb732cc53a50835b17023
Naveen B <naveen.dec29@gmail.com>
2023-11-20 22:27:15 -0500

    [refactor1] random commit

--- src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLazyInitExampleApplication {
 
 	public static void main(String[] args) {
+		//C
 		SpringApplication.run(SpringBootLazyInitExampleApplication.class, args);
+		//D
 	}
 
 }
