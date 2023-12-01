36fa2f29f80348e97952ef18555c5e5201ab5c0e
Naveen B <naveen.dec29@gmail.com>
2023-12-01 17:50:16 -0500

    test

--- src/main/java/com/naveen/lazy/LazyController.java
+++ src/main/java/com/naveen/lazy/LazyController.java
@@ -15,6 +15,6 @@ public class LazyController {
 
     @GetMapping  //d
     public String hello() {
-        return lazyService.hello();  //gree
+        return lazyService.hello();  //greeting updated.
     }
 }
