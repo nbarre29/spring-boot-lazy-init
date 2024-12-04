97a0ed0329089baa20f4385832b3ee5977202684
Naveen B <naveen.dec29@gmail.com>
2024-12-03 20:58:28 -0500

    added doc

--- src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/naveen/lazy/SpringBootLazyInitExampleApplication.java
@@ -3,7 +3,7 @@ package com.naveen.lazy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//main method
 @SpringBootApplication
 public class SpringBootLazyInitExampleApplication {
 
