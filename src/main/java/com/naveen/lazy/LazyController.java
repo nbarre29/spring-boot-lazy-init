4092a95c5c4206f0851c8f583a866a3a303edbd2
Naveen B <naveen.dec29@gmail.com>
2024-12-03 21:12:55 -0500

    removed comment

--- src/main/java/com/naveen/lazy/LazyController.java
+++ src/main/java/com/naveen/lazy/LazyController.java
@@ -15,6 +15,6 @@ public class LazyController {
 
     @GetMapping  //d
     public String hello() {
-        return lazyService.hello();  //greeting updated.
+        return lazyService.hello(); 
     }
 }
