2e452d743c7d1060329be32f035e4f8d80962337
Naveen B <naveen.dec29@gmail.com>
2024-12-06 09:15:48 -0500

    [main]

--- src/main/java/com/naveen/lazy/LazyController.java
+++ src/main/java/com/naveen/lazy/LazyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lazy")
+//class comment main
 public class LazyController {
        
     @Autowired
