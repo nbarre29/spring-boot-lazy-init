bc73f4377d553232d852736d9f88f7db3548194c
Naveen B <naveen.dec29@gmail.com>
2024-12-05 21:30:19 -0500

    test

--- src/main/java/com/naveen/lazy/LazyController.java
+++ src/main/java/com/naveen/lazy/LazyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LazyController {
 
 //    @Lazy
-    @Autowired  //c
+    @Autowired  //update comment
     LazyService lazyService;
 
     @GetMapping  //initial_abcn
